a691df73c768ff2474fe3bef59ba140297bdde9a
vexxoz <blakecaldwell123@gmail.com>
2018-07-04 19:48:19 -0700

    Generate Strings

---
+++ FIleGen.java
@@ -0,0 +1,25 @@
+
+public class FIleGen {
+
+	public static void main(String[] args) {
+	
+		run(4);
+
+	}
+
+	public static void run(int num) {
+		if(num == 0) {
+		
+		}else {
+			String word = "";
+			for(int i = 0; i<num; i++) {
+				
+			}
+			
+			run(num-1);
+		}
+		
+		
+	}
+	
+}
